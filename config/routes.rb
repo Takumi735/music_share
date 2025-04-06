9ffbcce7c22cd5463f07d02dd6693dded86d832d
Takumi735 <lzidyss@gmail,com>
2025-04-06 19:15:20 +0900

    search_controller作成

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ root "posts#index"
 
 resources :posts, only: [ :index, :show, :new, :create, :destroy ]
 
+get "search/index"
+
 devise_for :users
 devise_scope :user do
   get "/users/sign_out" => "devise/sessions#destroy"
