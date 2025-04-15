344f6b8c867a659aaae403705bf89908854b0ab1
Takumi735 <lzidyss@gmail,com>
2025-04-15 11:44:29 +0900

    search/indexスタイル変更、投稿のソート機能追加

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,14 @@
 class PostsController < ApplicationController
   def index
+    @posts = Post.all
+
+    if params[:artist_name].present?
+      @posts = @posts.where(artist_name: params[:artist_name])
+    end
+
+    if params[:song_title].present?
+      @posts = @posts.where(song_title: params[:song_title])
+    end
   end
 
   def show
