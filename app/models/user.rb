5f93ab6df72bddea19d03416316632b99e67fe15
Takumi735 <lzidyss@gmail,com>
2025-05-03 01:18:06 +0900

    userモデル :posts, dependent: :destroy追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
         :recoverable, :rememberable, :validatable
   validates :name, presence: true
 
-  has_many :posts
+  has_many :posts, dependent: :destroy
   has_many :likes, dependent: :destroy
   has_many :liked_posts, through: :likes, source: :post
 end
