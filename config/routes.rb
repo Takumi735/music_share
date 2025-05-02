b63ffaedb63c14957b6924d49d1b53aae0276a87
Takumi735 <lzidyss@gmail,com>
2025-05-03 01:16:31 +0900

    mypages_controller作成、ユーザー情報,post,like view作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,17 @@
 Rails.application.routes.draw do
-root "posts#index"
+  root "posts#index"
+  get "mypage", to: "mypages#show", as: "mypage"
+  get "search", to: "search#index"
+  get "tracks/search", to: "tracks#search", as: "search_tracks"
 
-resources :posts, only: [ :index, :show, :new, :create, :destroy ]
+  resources :posts, only: [ :index, :show, :new, :create, :destroy ]
 
-get "search", to: "search#index"
-
-devise_for :users
-devise_scope :user do
-  get "/users/sign_out" => "devise/sessions#destroy"
+  resources :posts do
+    resource :like, only: [ :create, :destroy ]
   end
 
-get "tracks/search", to: "tracks#search", as: "search_tracks"
-
-resources :posts do
-  resource :like, only: [ :create, :destroy ]
-end
+  devise_for :users
+  devise_scope :user do
+    get "/users/sign_out" => "devise/sessions#destroy"
+  end
 end
