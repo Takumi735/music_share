e8ce2c838fd4f3c56300efc40346767f12ccaa01
Takumi735 <lzidyss@gmail,com>
2025-05-18 05:55:16 +0900

    システムスペック実装、searchs_spec、comments_spec実装

--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -2,9 +2,9 @@ FactoryBot.define do
   factory :post do
     association :user
 
-    spotify_track_id { Faker::Music::GratefulDead.song }
-    song_title { Faker::Music.album }
-    artist_name { Faker::Music.band }
+    spotify_track_id { "5LkWXxDxUTwEVHL0pfKL55" }
+    song_title { "Pretender" }
+    artist_name { "Official髭男dism" }
     content { Faker::Lorem.sentence(word_count: 10) }
   end
 end
