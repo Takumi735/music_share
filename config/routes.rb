cfd2034f55b8a1a645b3d78ec331d05d0fd30247
Takumi735 <lzidyss@gmail,com>
2025-05-21 11:30:58 +0900

    ゲストログイン機能実装

--- config/routes.rb
+++ config/routes.rb
@@ -9,8 +9,12 @@ Rails.application.routes.draw do
     resources :comments, only: [ :create ]
   end
 
-  devise_for :users
+  devise_for :users, controllers: {
+    sessions: "users/sessions"
+  }
+
   devise_scope :user do
-    get "/users/sign_out" => "devise/sessions#destroy"
+    get "/users/sign_out", to: "devise/sessions#destroy"
+    post "users/guest_sign_in", to: "users/sessions#guest_sign_in"
   end
 end
